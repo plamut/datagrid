6c4c9ff00cea848c9811a8a1f58ae7bb1864f923
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-18 17:19:33 +0100

    Add accessor methods to Replica

--- src/models/replica.py
+++ src/models/replica.py
@@ -1,6 +1,14 @@
 class Replica(object):
-    """TODO: docstring"""
+    """A representation of a replica (file)."""
 
     def __init__(self, name='', size=100):
         self._name = name
         self._size = size  # TODO: check for > 0?
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def size(self):
+        return self._size
