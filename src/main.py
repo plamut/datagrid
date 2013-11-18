e6da51b890e398e5bbd21b64f6de5549ef5d787c
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-18 16:10:05 +0100

    Add main.py and models.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from models import Node
+
+
+def init_grid():
+    # TODO: ad some params
+    # TODO: set larger than replica amount
+    server = Node("Server Node", capacity=99999999, parent=None)
+
+    # TODO: what to return? some Grid object? and Grid.get_server ...
+
+
+
+def main():
+    print("Main function")
+
+    # XXX: have the main simulation code in a special class?
+
+    init_grid()
+
+
+    # run simulation
+
+    # print results
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/models.py
@@ -0,0 +1,50 @@
+from collections import OrderedDict
+
+
+class Node(object):
+    """TODO: docstring"""
+
+    def __init__(self, name='', capacity=50000, parent=None):
+        self._name = name
+        self._capacity = capacity  # XXX: check for > 0?
+        self._parent = parent
+        self._client_nodes = OrderedDict()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        # TODO:
+        self._parent = value
+
+    @property
+    def is_server(self):
+        return self._parent is None
+
+    def add_client_node(self, node):
+        # TODO: implement as a collection? e.g. self[] = node?
+        # overrdie __setitem__ and __getitem__ and __delitem__
+        self._client_nodes[node.name] = node
+        node.parent = self
+
+    # add_replica ... and algorithm (if size ...)
+
+
+
+
+class Replica(object):
+    """TODO: docstring"""
+
+    def __init__(self, name='', size=100):
+        self._name = name
+        self._size = size  # TODO: check for > 0?
