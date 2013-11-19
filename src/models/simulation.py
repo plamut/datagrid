13274196353d1ef6f828c4c099fa1fc40de5b060
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-19 11:13:32 +0100

    more flake8 ...

--- src/models/simulation.py
+++ src/models/simulation.py
@@ -15,13 +15,22 @@ class Simulation(object):
 
         self._replica_count = replica_count
         self._replicas = []
+        self._nodes = []
 
         # TODO: replica count, replica sizes
+
+    def _add_node(self, *args, **kwargs):
+        """TODO"""
+        node = Node(*args, **kwargs)
+        self._nodes.append(node)
+        return node
 
     def init_grid(self):
         """TODO"""
         # XXX: why does the simulation say that the number of nodes is 50,
         # but in the picture there are only 14 (including the server)?
+        self._replicas = []
+        self._nodes = []
 
         # generate replicas
         for i in range(self._replica_count):
@@ -32,24 +41,30 @@ class Simulation(object):
             self._replicas.append(replica)
 
         # TODO: set larger than replica amount
-        server = Node("Server Node", capacity=99999999, parent=None)
+        server = self._add_node("Server Node", capacity=99999999, parent=None)
 
-        node_3 = Node("node_3", capacity=self._node_capacity, parent=server)
-        node_1 = Node("node_1", capacity=self._node_capacity, parent=node_3)
-        node_2 = Node("node_2", capacity=self._node_capacity, parent=node_3)
+        node_3 = self._add_node(
+            "node_3", capacity=self._node_capacity, parent=server)
+        self._add_node("node_1", capacity=self._node_capacity, parent=node_3)
+        self._add_node("node_2", capacity=self._node_capacity, parent=node_3)
 
-        node_5 = Node("node_5", capacity=self._node_capacity, parent=server)
-        node_4 = Node("node_4", capacity=self._node_capacity, parent=server)
-        node_6 = Node("node_6", capacity=self._node_capacity, parent=node_4)
+        self._add_node("node_5", capacity=self._node_capacity, parent=server)
+        node_4 = self._add_node(
+            "node_4", capacity=self._node_capacity, parent=server)
+        self._add_node("node_6", capacity=self._node_capacity, parent=node_4)
 
-        node_7 = Node("node_7", capacity=self._node_capacity, parent=server)
-        node_9 = Node("node_9", capacity=self._node_capacity, parent=node_7)
-        node_12 = Node("node_12", capacity=self._node_capacity, parent=node_9)
+        node_7 = self._add_node(
+            "node_7", capacity=self._node_capacity, parent=server)
+        node_9 = self._add_node(
+            "node_9", capacity=self._node_capacity, parent=node_7)
+        self._add_node("node_12", capacity=self._node_capacity, parent=node_9)
 
-        node_8 = Node("node_8", capacity=self._node_capacity, parent=server)
-        node_10 = Node("node_10", capacity=self._node_capacity, parent=node_8)
-        node_11 = Node("node_11", capacity=self._node_capacity, parent=node_8)
-        node_13 = Node("node_13", capacity=self._node_capacity, parent=node_11)
+        node_8 = self._add_node(
+            "node_8", capacity=self._node_capacity, parent=server)
+        self._add_node("node_10", capacity=self._node_capacity, parent=node_8)
+        node_11 = self._add_node(
+            "node_11", capacity=self._node_capacity, parent=node_8)
+        self._add_node("node_13", capacity=self._node_capacity, parent=node_11)
 
         # TODO: what to return? some Grid object? and Grid.get_server ...
         # or just store all replicas for later access?
