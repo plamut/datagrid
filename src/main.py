81c9d230203d94b135466242a9d1fac3058fc97e
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-26 13:46:31 +0100

    Simulation gardening docstrings, param checking

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from models.simulation import Simulation
 
 
 sim_settings = dict(
+    node_count=20,
     node_capacity=50000,
     strategy=Strategy.EFS,
+    min_dist_km=1,  # min distance between two adjacent nodes
+    max_dist_km=1000,  # max distance between two adjacent nodes
     replica_count=1000,
+    replica_min_size=100,  # megabits
+    replica_max_size=1000,  # megabits
+    rnd_seed=1,
+    total_reqs=100000,
+    fste=1000,  # frequency specific time interval
 )
 
 
--- src/models/simulation.py
+++ src/models/simulation.py
@@ -15,6 +15,8 @@ Strategy = namedtuple('Strategy', [
     's2013',  # TODO
 ])
 Strategy = Strategy(*range(1, 4))
+"""Possible strategies for the nodes to use when deciding, which
+replica(s) (if any) to replace when locally storing a new replica."""
 
 
 class _Clock(object):
@@ -25,39 +27,95 @@ class _Clock(object):
 
     @property
     def time(self):
+        """Current simulation time."""
         return self._time
 
     def reset(self):
+        """Reset current simulation time to zero (0)."""
         self._time = 0
 
-    # XXX: add step parameter? for advancing for more than one unit?
     def tick(self, step=1):
+        """Increase current simulation time by `step`.
+
+        :param step: by how much to increase the time
+            (optional, default: 1)
+        :type step: int
+
+        :returns: new simulation time
+        :rtype: int
+        """
         if step < 1:
             raise ValueError("Clock step must be positive.")
         self._time += int(step)
+        return self._time
 
 
 # XXX: provide a SimulationView for Node objects? A "readonly view" of the
 # sim object for nodes to use
-class Simulation(object):
-    """TODO: docstring"""
 
-    RND_SEED = 1  # XXX configurable?
-    REPLICA_MIN_SIZE = 100  # megabits   XXX: make configurable?
-    REPLICA_MAX_SIZE = 1000  # megabits   XXX: make configurable?
-    TOTAL_REQUESTS = 100000  # simmulation steps   XXX: make configurable?
-    SERVER_NAME = 'server'
+class Simulation(object):
+    """Simulation runner."""
+
+    SERVER_NAME = 'server'  # XXX: make configurable?
 
     def __init__(
         self, node_capacity=50000, strategy=Strategy.EFS,
         replica_count=1000, node_count=20, fste=10000,
-        min_dist_km=1, max_dist_km=1000
+        min_dist_km=1, max_dist_km=1000,
+        replica_min_size=100, replica_max_size=1000,
+        rnd_seed=None, total_reqs=100000,
     ):
-        self._node_capacity = node_capacity
-        self._strategy = strategy  # TODO: convert to "enum"
+        """Initialize simulation parameters.
 
-        self._node_count = node_count  # XXX: check > 0
+        :param node_count: number of nodes in the grid including the server
+            node
+            (optional, default: 20)
+        :type node_count: int
+        :param node_capacity: capacity of each non-server node in megabits
+            (optional, default: 50000)
+        :type node_capacity: int
+        :param strategy: strategy to use for replica replacement
+            (optional, deafult: Strategy.EFS)
+        :type strategy: int
+        :param min_dist_km:  min distance between two adjacent nodes in
+            kilometers
+            (optional, default: 1)
+        :type min_dist_km: int
+        :param max_dist_km:  max distance between two adjacent nodes in
+            kilometers
+            (optional, default: 1)
+        :type max_dist_km: int
+        :param replica_count: number of different replicas in simulation
+            (optional, default: 1000)
+        :type replica_count: int
+        :param replica_min_size: min size of a single replica in megabits
+            (optional, default: 100)
+        :param replica_max_size: max size of a single replica in megabits
+            (optional, default: 1000)
+        :param total_reqs: number of replica requests to generate during the
+            simulation
+            (optional, default: 100000)
+        :type total_reqs: int
+        :param fste: frequency specific time interval as defined in the paper
+            (optional, default: 1000)
+        :type fste: int
+        :param rnd_seed: seed for the random number generator
+            (optional, default: current system time)
+        :type rnd_seed: int
+        """
+        self._node_capacity = node_capacity
+        self._strategy = strategy
+
+        if node_count < 2:
+            raise ValueError("Grid's node_count must be at least 2.")
+        self._node_count = node_count
+
+        if replica_count < 1:
+            raise ValueError("Number of replicas must be positive.")
         self._replica_count = replica_count
+
+        if fste < 1:
+            raise ValueError("FSTE must be positive.")
         self._fste = fste
 
         self._replicas = OrderedDict()
@@ -66,19 +124,48 @@ class Simulation(object):
 
         self._clock = _Clock()
 
-        # min and max distance between adjacent nodes
+        if min_dist_km >= max_dist_km:
+            raise ValueError("Min distance must be smaller than max distance")
+
+        if min_dist_km <= 0:
+            raise ValueError("Minimum distance must be positive.")
         self._min_dist_km = min_dist_km
+
+        if max_dist_km <= 0:
+            raise ValueError("Maximum distance must be positive.")
         self._max_dist_km = max_dist_km
-        # TODO: replica sizes
+
+        if replica_min_size >= replica_max_size:
+            raise ValueError(
+                "Min replica size must be smaller than max replica size.")
+
+        if replica_min_size <= 0:
+            raise ValueError("Minimum replica size must be positive.")
+        self._replica_min_size = replica_min_size
+
+        if replica_max_size <= 0:
+            raise ValueError("Maximum replica size must be positive.")
+        self._replica_max_size = replica_max_size
+
+        self._rnd_seed = rnd_seed
+
+        if total_reqs <= 0:
+            raise ValueError("Total number of requests must me be positive.")
+        self._total_reqs = total_reqs
 
     def _new_node(self, *args, **kwargs):
-        """TODO"""
+        """Create a new grid node and store it in the list of nodes.
+
+        :returns: newly created node instance
+        :rtype: :py:class:`~models.node.Node`
+        """
         node = Node(*args, **kwargs)
         self._nodes[node.name] = node
         return node
 
     @property
     def time(self):
+        """Current simulation time."""
         return self._clock.time
 
     # later of zero padding
@@ -87,13 +174,13 @@ class Simulation(object):
     # str = '0{}d'.format(digits)
 
     def _generate_nodes(self):
-        """ TODO """
+        """Generate a new set of grid nodes."""
         self._nodes = OrderedDict()
 
         self._new_node(
             self.SERVER_NAME,
             # server's capacity must be big enough to hold all replicas
-            capacity=self._replica_count * self.REPLICA_MAX_SIZE,
+            capacity=self._replica_count * self._replica_max_size,
             replicas=self._replicas,
             sim=self,
         )
@@ -106,8 +193,11 @@ class Simulation(object):
             )
 
     def _generate_edges(self):
-        """TODO"""
-        # generate edges - full graph with random edge distances
+        """Generate edges with random distances between grid nodes
+        (complete graph topology).
+
+        Edge distances lie in the interval [`min_dist_km`, `max_dist_km`].
+        """
         self._edges = OrderedDict()
 
         for node_name in self._nodes:
@@ -119,20 +209,29 @@ class Simulation(object):
             self._edges[node_2][node_1] = dist
 
     def _generate_replicas(self):
-        """TODO:docstring"""
+        """Generate `replica_count` replicas with random sizes.
+
+        Sizes lie in the interval [`replica_min_size`, `replica_max_size`].
+        """
         self._replicas = OrderedDict()
 
         for i in range(self._replica_count):
             replica = Replica(
                 name='replica_{}'.format(i),  # XXX: zero padding e.g. {03d}
-                size=randint(self.REPLICA_MIN_SIZE, self.REPLICA_MAX_SIZE)
+                size=randint(self._replica_min_size, self._replica_max_size)
             )
             self._replicas[replica.name] = replica
 
-    def _shortest_paths(self):
-        """Find shortest paths from server node to all other nodes.
+    def _dijkstra(self):
+        """Find the shortest paths from server node to all other nodes.
 
-        TODO: describe return value
+        :returns: node information as calculated by Dijkstra algorithm
+        :rtype: OrderedDict (key: node name, value: node info object)
+            Node info object is a SimpleNamespace instance with the
+            following attributes:
+            * dist: total distance from node to server node
+            * previous: previous node on the shortest path to server
+            * visited: always True (result of Dijsktra algorithm)
         """
         node_info = OrderedDict()
 
@@ -158,29 +257,35 @@ class Simulation(object):
 
             for v, dist_u_v in self._edges[u].items():  # all neighbors of u
                 alt = node_info[u].dist + dist_u_v
+
+                # if alternative path to v (through u) is smaller than the
+                # currently known best path to v, update v
                 if alt < node_info[v].dist and not node_info[v].visited:
                     node_info[v].dist = alt
                     node_info[v].previous = u
                     q.add(v)
 
         return node_info
-        # TODO: now backtrace node.previous and construct shortest paths
-        # (for every node) .. then return this (as a list for every node)
 
     def nsp_path(self, node):
-        """Return a list of node names on the shortest path from 'node' to
-        server node.
+        """For a given node, return a list of node names on the shortest path
+        from `node` to server node (including `node` itself).
+
+        :param node: the node for which we need its shortest path
+        :type node: :py:class:`~models.node.Node`
+
+        :returns: list of node names on the shortest path to server
+        :rtype: list of strings
         """
         return self._nsp_paths[node.name]
 
     def initialize(self):
-        """TODO"""
-        # XXX: why does the simulation say that the number of nodes is 50,
+        """Initialize (reset) a simulation."""
+        # XXX: why does the paper say that the number of nodes is 50,
         # but in the picture there are only 14 (including the server)?
-        self._clock.reset()
 
-        # TODO: make configurable (if None, then use random seed)
-        seed(self.RND_SEED)
+        self._clock.reset()
+        seed(self._rnd_seed)
 
         # generate nodes
         self._generate_replicas()
@@ -189,7 +294,7 @@ class Simulation(object):
 
         # calculate shortest paths from server node to all the other nodes
         # and update each of the latter with a relevant path
-        node_info = self._shortest_paths()
+        node_info = self._dijkstra()
         for name, info in node_info.items():
             shortest_path = [name]
             previous = info.previous
@@ -203,9 +308,12 @@ class Simulation(object):
         # print('PATHS', node_info, '\n')
 
     def run(self):
-        """TODO:"""
+        """Run simulation.
 
-        for i in range(self.TOTAL_REQUESTS):
+        NOTE: simulation must have already been initialized with initialize()
+        """
+
+        for i in range(self._total_reqs):
             # random time from 0 to 99 (params), advance clock,
             # generate event (node requests a replica)
             self._clock.tick()
