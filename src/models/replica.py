f363be7179e2254ac49d1f8ebca154e60862d765
Peter Lamut <peter.lamut@niteoweb.com>
2013-11-26 16:02:49 +0100

    Replica: add docstrings and check for 'size' > 0 in __init__

--- src/models/replica.py
+++ src/models/replica.py
@@ -1,14 +1,26 @@
 class Replica(object):
     """A representation of a replica (file)."""
 
-    def __init__(self, name='', size=100):
+    def __init__(self, name, size):
+        """Initialize replica instance.
+
+        :param name: replica name
+        :type name: string
+        :param size: replica size in megabits
+        :type size: int
+        """
         self._name = name
-        self._size = size  # TODO: check for > 0?
+
+        if size <= 0:
+            raise ValueError("Size must be positive.")
+        self._size = size
 
     @property
     def name(self):
+        """Replica's name."""
         return self._name
 
     @property
     def size(self):
+        """Size of the replica in megabits."""
         return self._size
