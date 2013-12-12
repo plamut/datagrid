0f407ece7e6e06855110a17686d1d3bf907ef3fd
Peter Lamut <peter.lamut@niteoweb.com>
2013-12-12 18:27:36 +0100

    Add tests for Simulation.__init__()

--- src/tests/test_simulation.py
+++ src/tests/test_simulation.py
@@ -1,5 +1,7 @@
 """Tests for :py:mod:`models.simulation` module."""
 
+from collections import OrderedDict
+
 import unittest
 
 
@@ -114,3 +116,174 @@ class TestClock(unittest.TestCase):
         clock = self._make_instance()
         with self.assertRaises(ValueError):
             clock.tick(step=-1.0)
+
+
+class TestSimulation(unittest.TestCase):
+    """Tests for :py:class:`models.simulation.Simulation`."""
+
+    def _get_target_class(self):
+        from models.simulation import Simulation
+        return Simulation
+
+    def _make_instance(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def _get_settings(self):
+        """Get example simulation configuration settings.
+
+        :returns: An example of simulation configuration settings.
+        :rtype: dict
+        """
+        from models.simulation import Strategy
+        return dict(
+            node_capacity=12000,
+            strategy=Strategy.EFS,
+            node_count=14,
+            replica_count=100,
+            fsti=500,  # frequency specific time interval
+            min_dist_km=5,  # min distance between two adjacent nodes
+            max_dist_km=800,  # max distance between two adjacent nodes
+            network_bw_mbps=20,
+            pspeed_kmps=7e3,
+            replica_min_size=70,  # megabits
+            replica_max_size=700,  # megabits
+            rnd_seed=42,
+            total_reqs=10000,
+        )
+
+    def test_name_for_server_node(self):
+        """Test that predefined name of the server node is correct."""
+        name = self._get_target_class().SERVER_NAME
+        self.assertEqual(name, 'server')
+
+    def test_init(self):
+        """Test that new instances are correctly initialized."""
+        settings = self._get_settings()
+        sim = self._make_instance(**settings)
+
+        self.assertEqual(sim._node_capacity, settings['node_capacity'])
+        self.assertEqual(sim._strategy, settings['strategy'])
+        self.assertEqual(sim._node_count, settings['node_count'])
+        self.assertEqual(sim._replica_count, settings['replica_count'])
+        self.assertEqual(sim._fsti, settings['fsti'])
+        self.assertEqual(sim._min_dist_km, settings['min_dist_km'])
+        self.assertEqual(sim._max_dist_km, settings['max_dist_km'])
+        self.assertEqual(sim._network_bw_mbps, settings['network_bw_mbps'])
+        self.assertEqual(sim._pspeed_kmps, settings['pspeed_kmps'])
+        self.assertEqual(sim._replica_min_size, settings['replica_min_size'])
+        self.assertEqual(sim._replica_max_size, settings['replica_max_size'])
+        self.assertEqual(sim._rnd_seed, settings['rnd_seed'])
+        self.assertEqual(sim._total_reqs, settings['total_reqs'])
+
+        self.assertEqual(sim._replicas, OrderedDict())
+        self.assertEqual(sim._nodes, OrderedDict())
+        self.assertEqual(sim._edges, OrderedDict())
+
+        from models.simulation import _Clock
+        self.assertTrue(isinstance(sim._clock, _Clock))
+
+        self.assertEqual(sim._total_bw, 0.0)
+        self.assertEqual(sim._total_rt_s, 0.0)
+        self.assertEqual(sim._c1, 0.001)
+        self.assertEqual(sim._c2, 0.001)
+
+    def test_init_checks_node_count_big_enough(self):
+        """Test that init checks that `node_count` is at least two (2)."""
+        settings = self._get_settings()
+        settings['node_count'] = 1
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+        settings['node_count'] = 2
+        try:
+            self._make_instance(**settings)
+        except:
+            self.fail("Number of nodes (2) should have been accepted.")
+
+    def test_init_rejects_non_positive_replica_count(self):
+        """Test that init rejects non-positive values for `replica_count`."""
+        settings = self._get_settings()
+        settings['replica_count'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_fsti(self):
+        """Test that init rejects non-positive values for `fsti`."""
+        settings = self._get_settings()
+        settings['fsti'] = 0.0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_min_dist_km(self):
+        """Test that init rejects non-positive values for `min_dist_km`."""
+        settings = self._get_settings()
+        settings['min_dist_km'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_max_dist_km(self):
+        """Test that init rejects non-positive values for `max_dist_km`."""
+        settings = self._get_settings()
+        settings['max_dist_km'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_checks_max_dist_is_greater_than_min_dist(self):
+        """Test that init checks that `max_dist_km` is greater than
+        `min_dist_km`.
+        """
+        settings = self._get_settings()
+        settings['min_dist_km'] = settings['max_dist_km']
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_network_bw_mbps(self):
+        """Test that init rejects non-positive values for `network_bw_mbps`."""
+        settings = self._get_settings()
+        settings['network_bw_mbps'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_replica_min_size(self):
+        """Test that init rejects non-positive values for `replica_min_size`.
+        """
+        settings = self._get_settings()
+        settings['replica_min_size'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_replica_max_size(self):
+        """Test that init rejects non-positive values for `replica_max_size`.
+        """
+        settings = self._get_settings()
+        settings['replica_max_size'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_checks_replica_max_size_is_greater_than_min_size(self):
+        """Test that init checks that `replica_max_size` is greater than
+        `replica_min_size`.
+        """
+        settings = self._get_settings()
+        settings['replica_min_size'] = settings['replica_max_size']
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
+
+    def test_init_rejects_non_positive_total_reqs(self):
+        """Test that init rejects non-positive values for `total_reqs`.
+        """
+        settings = self._get_settings()
+        settings['total_reqs'] = 0
+
+        with self.assertRaises(ValueError):
+            self._make_instance(**settings)
